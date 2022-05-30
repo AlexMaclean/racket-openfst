34c5207130b6f9bba75ee6476a55ec54fad25691
Alex Maclean (WSL) <alex.maclean2000@gmail.com>
2022-05-30 14:13:07 -0700

    windows

--- lib/openfst_wrapper.cc
+++ lib/openfst_wrapper.cc
@@ -1,103 +1,106 @@
 #include <fst/fstlib.h>
 
+#ifdef _WIN32
+    #define DllExport __declspec( dllexport )
+    #define TOKEN StringTokenType::UTF8
+#else
+    #define DllExport
+    #define TOKEN TokenType::UTF8
+#endif
+
 using namespace fst;
 
 extern "C"
 {
 
-    StdVectorFst *new_Fst()
+    DllExport StdVectorFst *new_Fst()
     {
         return new StdVectorFst();
     }
 
-    StdVectorFst *new_Fst_copy(fst::StdFst *fst)
+    DllExport StdVectorFst *new_Fst_copy(fst::StdFst *fst)
     {
         return new StdVectorFst(*fst);
     }
 
-    void Fst_AddArc(fst::StdMutableFst *fst, int state, StdArc *arc)
+    DllExport void Fst_AddArc(fst::StdMutableFst *fst, int state, StdArc *arc)
     {
         fst->AddArc(state, *arc);
     }
 
-    int Fst_AddState(fst::StdMutableFst *fst)
+    DllExport int Fst_AddState(fst::StdMutableFst *fst)
     {
         return fst->AddState();
     }
 
-    void Fst_AddStates(fst::StdMutableFst *fst, size_t n)
-    {
-        fst->AddStates(n);
-    }
-
-    int Fst_NumStates(fst::StdMutableFst *fst)
+    DllExport int Fst_NumStates(fst::StdMutableFst *fst)
     {
         return fst->NumStates();
     }
 
-    size_t Fst_NumArcs(fst::StdFst *fst, int state)
+    DllExport size_t Fst_NumArcs(fst::StdFst *fst, int state)
     {
         return fst->NumArcs(state);
     }
 
-    void Fst_SetStart(fst::StdMutableFst *fst, int state)
+    DllExport void Fst_SetStart(fst::StdMutableFst *fst, int state)
     {
         fst->SetStart(state);
     }
 
-    void Fst_SetFinal(fst::StdMutableFst *fst, int state, float weight)
+    DllExport void Fst_SetFinal(fst::StdMutableFst *fst, int state, float weight)
     {
         fst->SetFinal(state, weight);
     }
 
-    void Fst_Write(fst::StdFst *fst, char *path)
+    DllExport void Fst_Write(fst::StdFst *fst, char *path)
     {
         fst->Write(path);
     }
 
-    int Fst_Start(fst::StdFst *fst)
+    DllExport int Fst_Start(fst::StdFst *fst)
     {
         return fst->Start();
     }
 
-    float Fst_Final(fst::StdFst *fst, int state)
+    DllExport float Fst_Final(fst::StdFst *fst, int state)
     {
         return fst->Final(state).Value();
     }
 
-    fst::StdMutableFst *Fst_Read(char *path)
+    DllExport fst::StdMutableFst *Fst_Read(char *path)
     {
         return fst::StdMutableFst::Read(path);
     }
 
-    const SymbolTable *Fst_InputSymbols(fst::StdFst *fst)
+    DllExport const SymbolTable *Fst_InputSymbols(fst::StdFst *fst)
     {
         return fst->InputSymbols();
     }
 
-    const SymbolTable *Fst_OutputSymbols(fst::StdFst *fst)
+    DllExport const SymbolTable *Fst_OutputSymbols(fst::StdFst *fst)
     {
         return fst->OutputSymbols();
     }
 
-    fst::StdMutableFst *Fst_Project(fst::StdFst *fst, ProjectType projectType)
+    DllExport fst::StdMutableFst *Fst_Project(fst::StdFst *fst, ProjectType projectType)
     {
         return new StdVectorFst(StdProjectFst(*fst, projectType));
     }
 
     // Symbol Table -------------------------------------------------------
 
-    size_t SymbolTable_NumSymbols(const SymbolTable *table)
+    DllExport size_t SymbolTable_NumSymbols(const SymbolTable *table)
     {
         return table->NumSymbols();
     }
 
-    int64_t SymbolTable_GetNthKey(const SymbolTable *table, size_t pos)
+    DllExport int64_t SymbolTable_GetNthKey(const SymbolTable *table, size_t pos)
     {
         return table->GetNthKey(pos);
     }
 
-    const char *SymbolTable_Find(SymbolTable *table, int64_t key)
+    DllExport const char *SymbolTable_Find(SymbolTable *table, int64_t key)
     {
         std::string *s = new std::string(table->Find(key));
         return s->c_str();
@@ -105,12 +108,12 @@ extern "C"
 
     // String Compiler -------------------------------------------------------
 
-    StdStringCompiler *new_StringCompiler()
+    DllExport StringCompiler<StdArc> *new_StringCompiler()
     {
-        return new StdStringCompiler();
+        return new StringCompiler<StdArc>(TOKEN);
     }
 
-    fst::StdMutableFst *StringCompiler_call(StdStringCompiler *compiler, char *str, float weight)
+    DllExport fst::StdMutableFst *StringCompiler_call(StringCompiler<StdArc> *compiler, char *str, float weight)
     {
         StdVectorFst *fst = new StdVectorFst();
         (*compiler)(str, fst, weight);
@@ -119,12 +122,12 @@ extern "C"
 
     // String Printer -------------------------------------------------------
 
-    StdStringPrinter *new_StringPrinter()
+    DllExport StringPrinter<StdArc> *new_StringPrinter()
     {
-        return new StdStringPrinter();
+        return new StringPrinter<StdArc>(TOKEN);
     }
 
-    const char *StringPrinter_call(StdStringPrinter *printer, fst::StdFst *fst)
+    DllExport const char *StringPrinter_call(StringPrinter<StdArc> *printer, fst::StdFst *fst)
     {
         std::string *str = new std::string();
         (*printer)(*fst, str);
@@ -133,19 +136,19 @@ extern "C"
 
     // Scripts -------------------------------------------------------
 
-    fst::StdMutableFst *Fst_ShortestPath(const fst::StdFst *fst, int32_t n_shortest)
+    DllExport fst::StdMutableFst *Fst_ShortestPath(const fst::StdFst *fst, int32_t n_shortest)
     {
         StdVectorFst *out_fst = new StdVectorFst();
         ShortestPath(*fst, out_fst, n_shortest);
         return out_fst;
     }
 
-    fst::StdMutableFst *Fst_Union(const fst::StdFst *fst1, const fst::StdFst *fst2)
+    DllExport fst::StdMutableFst *Fst_Union(const fst::StdFst *fst1, const fst::StdFst *fst2)
     {
         return new StdVectorFst(StdUnionFst(*fst1, *fst2));
     }
 
-    fst::StdMutableFst *Fst_Compose(fst::StdMutableFst *ifst1, fst::StdMutableFst *ifst2)
+    DllExport fst::StdMutableFst *Fst_Compose(fst::StdMutableFst *ifst1, fst::StdMutableFst *ifst2)
     {
         StdVectorFst *ofst = new StdVectorFst();
         ArcSort(ifst1, StdOLabelCompare());
@@ -154,15 +157,15 @@ extern "C"
         return ofst;
     }
 
-    fst::StdMutableFst *Fst_Cross(fst::StdFst *ifst1, fst::StdFst *ifst2)
+    DllExport fst::StdMutableFst *Fst_Cross(fst::StdFst *ifst1, fst::StdFst *ifst2)
     {
         StdVectorFst *ofst = new StdVectorFst();
         static const ComposeOptions opts(true, MATCH_FILTER);
         static const OutputEpsilonMapper<fst::StdArc> oeps;
         static const InputEpsilonMapper<fst::StdArc> ieps;
 
-        Compose(StdRmEpsilonFst(ArcMapFst(*ifst1, oeps)),
-                StdRmEpsilonFst(ArcMapFst(*ifst2, ieps)), ofst, opts);
+        Compose(StdRmEpsilonFst(ArcMapFst<StdArc, StdArc, OutputEpsilonMapper<StdArc>>(*ifst1, oeps)),
+            StdRmEpsilonFst(ArcMapFst<StdArc, StdArc, InputEpsilonMapper<StdArc>>(*ifst2, ieps)), ofst, opts);
 
         ofst->SetInputSymbols(ifst1->InputSymbols());
         ofst->SetOutputSymbols(ifst2->OutputSymbols());
@@ -170,12 +173,12 @@ extern "C"
         return ofst;
     }
 
-    fst::StdMutableFst *Fst_Concat(fst::StdMutableFst *ifst1, fst::StdMutableFst *ifst2)
+    DllExport fst::StdMutableFst *Fst_Concat(fst::StdMutableFst *ifst1, fst::StdMutableFst *ifst2)
     {
         return new StdVectorFst(StdConcatFst(*ifst1, *ifst2));
     }
 
-    fst::StdMutableFst *Fst_Difference(fst::StdMutableFst *ifst1, fst::StdMutableFst *ifst2)
+    DllExport fst::StdMutableFst *Fst_Difference(fst::StdMutableFst *ifst1, fst::StdMutableFst *ifst2)
     {
         StdVectorFst *ofst = new StdVectorFst();
         Difference(*ifst1, *ifst2, ofst);
@@ -184,71 +187,71 @@ extern "C"
 
     // Arc -------------------------------------------------------
 
-    fst::StdArc *new_Arc(int ilabel, int olabel, float weight, int dest)
+    DllExport fst::StdArc *new_Arc(int ilabel, int olabel, float weight, int dest)
     {
         return new fst::StdArc(ilabel, olabel, weight, dest);
     }
 
-    int Arc_ilabel(fst::StdArc *arc)
+    DllExport int Arc_ilabel(fst::StdArc *arc)
     {
         return arc->ilabel;
     }
 
-    int Arc_olabel(fst::StdArc *arc)
+    DllExport int Arc_olabel(fst::StdArc *arc)
     {
         return arc->olabel;
     }
 
-    int Arc_weight(fst::StdArc *arc)
+    DllExport int Arc_weight(fst::StdArc *arc)
     {
         return arc->weight.Value();
     }
 
-    int Arc_nextstate(fst::StdArc *arc)
+    DllExport int Arc_nextstate(fst::StdArc *arc)
     {
         return arc->nextstate;
     }
 
     // State Iterator -------------------------------------------------------
 
-    fst::StateIterator<fst::StdFst> *new_StateIterator(fst::StdMutableFst *fst)
+    DllExport fst::StateIterator<fst::StdFst> *new_StateIterator(fst::StdMutableFst *fst)
     {
         return new StateIterator<fst::StdFst>(*fst);
     }
 
-    int StateIterator_Value(fst::StateIterator<fst::StdFst> *siter)
+    DllExport int StateIterator_Value(fst::StateIterator<fst::StdFst> *siter)
     {
         return siter->Value();
     }
 
-    void StateIterator_Next(fst::StateIterator<fst::StdFst> *siter)
+    DllExport void StateIterator_Next(fst::StateIterator<fst::StdFst> *siter)
     {
         siter->Next();
     }
 
-    bool StateIterator_Done(fst::StateIterator<fst::StdFst> *siter)
+    DllExport bool StateIterator_Done(fst::StateIterator<fst::StdFst> *siter)
     {
         return siter->Done();
     }
 
     // Arc Iterator -------------------------------------------------------
 
-    fst::ArcIterator<fst::StdFst> *new_ArcIterator(fst::StdMutableFst *fst, int state)
+    DllExport fst::ArcIterator<fst::StdFst> *new_ArcIterator(fst::StdMutableFst *fst, int state)
     {
         return new ArcIterator<fst::StdFst>(*fst, state);
     }
 
-    fst::StdArc *ArcIterator_Value(fst::ArcIterator<fst::StdFst> *aiter)
+    DllExport fst::StdArc *ArcIterator_Value(fst::ArcIterator<fst::StdFst> *aiter)
     {
         return new fst::StdArc(aiter->Value());
     }
 
-    void ArcIterator_Next(fst::ArcIterator<fst::StdFst> *aiter)
+    DllExport void ArcIterator_Next(fst::ArcIterator<fst::StdFst> *aiter)
     {
         aiter->Next();
     }
 
-    bool ArcIterator_Done(fst::ArcIterator<fst::StdFst> *aiter)
+    DllExport bool ArcIterator_Done(fst::ArcIterator<fst::StdFst> *aiter)
     {
         return aiter->Done();
     }
